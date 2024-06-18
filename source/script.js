29f598ca12fbc94a8e5f0dbae3b96368cb1bd490
kubsterman <kubsterman2@gmail.com>
2024-06-18 17:53:47 +0200

    bug fixes

--- source/script.js
+++ source/script.js
@@ -1,38 +1,72 @@
-// THIS NEEDS A LOCAL SERVER ;( 
-// import data from 'data.json' assert {type: 'json'};
-//     console.log(data)
-
 document.addEventListener('DOMContentLoaded', function() {
     
     //temp placeholder data
-    const data = [
-        {label: "Poland", value: 40},
-        {label: "Tilted Towers", value: 55},
-        {label: "Lorum Ipsum", value: 75},
-        {label: "Salty Springs", value: 60},
-        {label: "Moisty Mires", value: 80},
-        {label: "Chorzów", value: 70}
+    const dataUS2021 = [
+        {"label": "BAC: 0.0", "value": 45769},
+        {"label": "BAC: 0.1 - 0.7", "value": 2373},
+        {"label": "BAC: 0.8+", "value": 12762}
     ];
-
+    const USlabels = dataUS2021.map(item => item.label);
+    const USvalues = dataUS2021.map(item => item.value);
     
-    //get data
-    const labels = data.map(item => item.label);
-    const values = data.map(item => item.value);
+
+    const dataUK2021 = [
+        {"label": "2021", "value": 6740},
+        {"label": "2020", "value": 6450},
+        {"label": "2019", "value": 7800},
+        {"label": "2018", "value": 8700},
+        {"label": "2017", "value": 8600},
+        {"label": "2016", "value": 9030}
+    ];
+    const UKlabels = dataUK2021.map(item => item.label);
+    const UKvalues = dataUK2021.map(item => item.value);
+
+    const colors = ['#caf0f8', '#ade8f4', '#90e0ef']; 
+    
 
     //initiate canvas and graph
     const ctx = document.getElementById('barchart').getContext('2d');
-    const BarChart = new Chart(ctx, {
+    const USChart = new Chart(ctx, {
+    
         //graph properties
-        type: 'bar',
+        type: 'pie',
         data: {
-            labels: labels,
+            
+            labels: USlabels,
             datasets: [{
-                label: 'car crashes :(',
-                data: values,
-                backgroundColor: '#A89B9D',
+                label: 'Fatal car crashes in the USA in 2021',
+                data: USvalues,
+                backgroundColor: colors,
                 borderColor: '#816C61',
                 borderWidth: 1
-            
+            }]
+        },
+        options: {
+            plugins: {
+                colors: {
+                  enabled: true
+                }
+              },
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+    
+    const ctx2 = document.getElementById('linechart').getContext('2d');
+    const lineChart = new Chart(ctx2, {
+        //graph properties
+        type: 'line',
+        data: {
+            labels: UKlabels,
+            datasets: [{
+                label: 'Fatal car crashes in the UK in 2021',
+                data: UKvalues,
+                backgroundColor: colors,
+                borderColor: '#816C61',
+                borderWidth: 1
             }]
         },
         options: {
